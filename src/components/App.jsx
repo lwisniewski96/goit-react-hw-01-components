7855b3ac95fadec7bcbdbce0b24c7c470d8e023e
lwisniewski96 <113308030+lwisniewski96@users.noreply.github.com>
2023-08-23 20:24:09 +0200

    test zad 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,32 @@
+import user from './Profile/user.json';
+import { Profile } from './Profile/Profile';
 
+// import data from './Statistics/data.json';
+// import { Statistics } from './Statistics/Statistics';
 
-
-
-
-
+const rootStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '40px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 40,
+  color: '#010101',
+};
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={rootStyles}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      {/* <Statistics title="Upload stats" stats={data} /> */}
     </div>
   );
 };
